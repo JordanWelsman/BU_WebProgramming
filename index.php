6f39d4d124bd2c884cc476cebe65fd44b7734ceb
JordanWelsman <jordan.welsman@hotmail.co.uk>
2021-10-09 22:01:20 +0100

    Edited index.php and added main stylesheet.

--- index.php
+++ index.php
@@ -4,9 +4,13 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/main.css">
     <title>Welcome to POSTS!</title>
 </head>
-<body>
-    <h1>Welcome to <strong>POSTS</strong>!</h1>
+<body class="background--dark">
+    <div class="card card--dark">
+        <h1>Welcome to <strong>POSTS</strong></h1>
+        <p>Properly Organised Shipment Tracking System</p>
+    </div>   
 </body>
 </html>
