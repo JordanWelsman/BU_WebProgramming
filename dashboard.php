2811418d32820045ba828f498c40c604286c2f62
JordanWelsman <jordan.welsman@hotmail.co.uk>
2021-10-13 01:22:33 +0100

    Made a lot of changes ot the dashboard and split CSS files into manageable chunks.

--- dashboard.php
+++ dashboard.php
@@ -4,30 +4,31 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/color.css">
     <link rel="stylesheet" href="css/dashboard.css">
+    <link rel="stylesheet" href="css/button.css">
     <link rel="shortcut icon" type="image/x-icon" href="ico/favicon.ico"/>
     <title>POSTS | Dashboard</title>
 </head>
 <body class="background--light" id="pageBody">
-    <nav>
-        <div class="wrapper">
-            <a href="../index.php"><img src="../ico/favicon-32x32.png" alt="POSTS Logo"></a>
-            <ul>
-                <button class="button--secondary button--small">➕</button>
-                <li><a href="dashboard.php">Dashboard</a></li>
-                <li><a href="account.php">Account</a></li>
+    <header>
+        <a class="logo" href="index.php"><img src="https://picsum.photos/200/50" alt="Site Logo"></a>
+        <a class="cta" href="#"><button class="button button--primary">Contact</button></a>
+        <nav>
+            <ul class="nav__links">
+                <li><a href="#">Services</a></li>
+                <li><a href="#">Projects</a></li>
+                <li><a href="#">About</a></li>
             </ul>
-        </div>
-    </nav>
-
-    <div class="wrapper">
-        <div class="pageContent">
-            <div class="topLeft"></div>
-            <div class="topRight"></div>
-            <div class="bottomLeft"></div>
-            <div class="bottomRight"></div>
-        </div>
+        </nav>
+        <a class="thc" href="#"><button class="button--secondary button--small" id="themeToggle">🕶️</button></a>
+    </header>
+    <div class="page__content">
+        <h1>Hello</h1>
+        <p>My name is Jordan Welsman and I'm a Data Science & Analytics student.</p>
     </div>
 </body>
+<footer>
+    <script src="js/scripts.js"></script>
+</footer>
 </html>
